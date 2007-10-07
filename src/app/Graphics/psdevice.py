103d235954e601c8c7cef1dfe2d132720f78e435
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2007-10-07 02:16:05 +0000

    debug messaging removed

--- src/app/Graphics/psdevice.py
+++ src/app/Graphics/psdevice.py
@@ -295,7 +295,6 @@ class PostScriptDevice(CommonDevice):
 
 	def _set_color(self, color):
 		if self.current_color != color:
-			print color
 			if color.model == 'CMYK':
 				c, m, y, k = color.getCMYK()
 				self.file.write('%g %g %g %g cmyk\n' % (round(c, 3), round(m, 3), round(y, 3), round(k, 3)))
