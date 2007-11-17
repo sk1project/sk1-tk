625ce489ad3ca4ec1d4178124a25799d85d2d096
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2007-11-17 20:47:01 +0000

    convertion to package

---
+++ src/__init__.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2007 by Igor E. Novikov
+#
+# This library is covered by GNU Library General Public License.
+# For more info see COPYRIGHTS file in root directory.
+
+'''
+USAGE: sk1 [INPUT FILE]
+
+sK1 is an open source vector graphics editor similar to CorelDRAW, Adobe Illustrator, or Freehand. 
+First of all sK1 is oriented for prepress industry.
+sK1 Team (http://sk1project.org), copyright (C) 2007 by Igor E. Novikov.
+
+ Allowed input formats:
+     AI  - Adobe Illustrator files (postscript based)
+     CDR - CorelDRAW Graphics files (7-X3 versions)
+     CDT - CorelDRAW templates files (7-X3 versions)
+     CCX - Corel Compressed Exchange files
+     CMX - Corel Presentation Exchange files (CMX1 format)
+     SVG - Scalable Vector Graphics files
+     FIG - XFig files
+     CGM - Computer Graphics Metafile files
+     AFF - Draw files
+     WMF - Windows Metafile files
+     SK  - Sketch/Skencil files
+     SK1 - sK1 vector graphics files
+
+Example: sk1 drawing.cdr
+'''
+
+import sys, os
+
+if sys.argv[1]=='--help':
+	print __doc__
+	sys.exit(0)
+
+_pkgdir = __path__[0]
+sys.path.insert(1, _pkgdir)
+
+import sys, app
+
+app.config.sk_command = sys.argv[0]
+app.main.main()
