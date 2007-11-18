90dcc7c5d2e65cf6f3095d18c1f56e14898a3c99
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2007-11-18 01:09:57 +0000

    old unwanted modules are removed (goodby Sketch! :) )

--- src/modules/pax-0.6.0/paxutil.h
+++
@@ -1,9 +0,0 @@
-#ifndef PAXUTIL_H
-#define PAXUTIL_H
-
-int pax_checkshortlist(int width, PyObject *list,
-		       short **parray, int *pnitems);
-
-int pax_checkdoublelist(int width, PyObject *list,
-		       double **parray, int *pnitems);
-#endif /* PAXUTIL_H */
