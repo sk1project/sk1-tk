cd0360e1efed30f94a106fbd9bf83138a2ed5c6a
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2007-12-03 09:49:14 +0000

    source changes for context panel

---
+++ src/app/UI/context/rotation_panel.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2003-2006 by Igor E. Novikov
+# Copyright (C) 1997, 1998, 1999, 2000, 2001, 2002, 2003 by Bernhard Herzog
+#
+# This library is covered by GNU Library General Public License.
+# For more info see COPYRIGHTS file in sK1 root directory.
+
+from app.UI.Ttk import TLabel, TButton
+from app.UI.ttk_ext import TSpinbox
+from app.conf.const import SELECTION
+from Tkinter import LEFT, RIGHT, DoubleVar
+from subpanel import CtxSubPanel
+from app import  _
+from app.UI import tooltips
+
+class RotatePanel(CtxSubPanel):
+	
+	name='RotatePanel'	
+	
+	def __init__(self, parent):
+		CtxSubPanel.__init__(self, parent)
+		self.angle=DoubleVar(self.parent.mainwindow.root)
+		self.angle.set(0)
+		
+		label = TLabel(self.panel, text=_(" Rotate: "))
+		label.pack(side = LEFT)
+		self.entry_width = TSpinbox(self.panel,  var=0, vartype=1, textvariable = self.angle,
+						min = -360, max = 360, step = 1, width = 6, command = self.applyRotate)
+		self.entry_width.pack(side = LEFT)
+		mw=self.parent.mainwindow
+		b = TButton(self.panel, command=self.rotLeft, style='Toolbutton', image='context_rotate_ccw')
+		tooltips.AddDescription(b, 'Rotate -90°')
+		b.pack(side = LEFT)
+		b = TButton(self.panel,  command=self.rot180, style='Toolbutton', image='context_rotate')
+		tooltips.AddDescription(b, 'Rotate 180°')
+		b.pack(side = LEFT)
+		b = TButton(self.panel,  command=self.rotRight, style='Toolbutton', image='context_rotate_cw')
+		tooltips.AddDescription(b, 'Rotate 90°')
+		b.pack(side = LEFT)
+
+	def rot180(self):
+		self.rotation(180)
+		
+	def rotLeft(self):
+		self.rotation(-90)
+		
+	def rotRight(self):
+		self.rotation(90)
+		
+	def applyRotate(self,  event):
+		self.rotation(self.angle.get())
+		
+	def rotation(self, angle):
+		self.parent.mainwindow.document.RotateSelected(angle)
