1cee14a7e610296bda581673aa9b01e1a0ade6ff
maxedrom <maxedrom@f60f84d3-f428-0410-86cb-e149298a0537>
2008-09-19 07:45:25 +0000

    Fix style checkbuttons in eXPect theme

--- src/share/ttk-themes/eXPect/styles/checkbuttons.tcl
+++ src/share/ttk-themes/eXPect/styles/checkbuttons.tcl
@@ -4,8 +4,8 @@
 				{!active  !disabled  selected !pressed} $K(check_yes_normal) \
 				{ active  !disabled  selected !pressed} $K(check_yes_active) \
 				{ active  !disabled !selected !pressed} $K(check_no_active) \
-				{!active  !disabled  selected  pressed} $K(check_yes_pressed) \
-				{!active  !disabled !selected  pressed} $K(check_no_pressed) \
+				{ active  !disabled  selected  pressed} $K(check_yes_pressed) \
+				{ active  !disabled !selected  pressed} $K(check_no_pressed) \
 				{disabled  selected} $K(check_yes_disabled) \
 				{disabled !selected} $K(check_no_disabled) ] \
 				-width 20 -sticky w 
