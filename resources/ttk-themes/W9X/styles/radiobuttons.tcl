e6b871a3f936ebdccea5f77bbabefb76dce60848
maxedrom <maxedrom@f60f84d3-f428-0410-86cb-e149298a0537>
2008-09-23 08:17:02 +0000

    W9X theme initial commit

---
+++ resources/ttk-themes/W9X/styles/radiobuttons.tcl
@@ -0,0 +1,50 @@
+# -------------------------------------Radiobutton---------------------------------------------------
+        style element create Radiobutton.indicator image [list $K(radio_no_normal) \
+				{!active  !disabled !selected !pressed} $K(radio_no_normal) \
+				{!active  !disabled  selected !pressed} $K(radio_yes_normal) \
+				{ active  !disabled  selected !pressed} $K(radio_yes_active) \
+				{ active  !disabled !selected !pressed} $K(radio_no_active) \
+				{!active  !disabled  selected  pressed} $K(radio_yes_pressed) \
+				{!active  !disabled !selected  pressed} $K(radio_no_pressed) \
+				{disabled  selected} $K(radio_yes_disabled) \
+				{disabled !selected} $K(radio_no_disabled) ] \
+				-width 20 -sticky w 
+			
+
+# ----------------------------------FineRadiobutton-------------------------------------------- 			
+        style layout FineRadiobutton {
+            FineRadiobutton.indicator -children {
+	    	FineRadiobutton.focus -children {
+                   FineRadiobutton.label -sticky ns
+		}    
+            }
+        }			
+        style element create FineRadiobutton.indicator image [list $K(fine_normal)\
+				{!active  !disabled !selected} $K(fine_normal) \
+				{!active  !disabled selected} $K(fine_selected) \
+				{active  !disabled selected} $K(fine_selected) \
+				{active  !disabled !selected} $K(fine_active) \
+				{disabled selected} $K(fine_selected) \
+				{disabled !selected} $K(fine_normal) ] \
+				-border {2 2 2 2} \
+				-padding {3 3 3 3}
+#				-width 33 -height 33  
+				
+# ----------------------------------ToolbarRadiobutton-------------------------------------------- 			
+style layout ToolbarRadiobutton {
+	ToolbarRadiobutton.indicator -children {
+	ToolbarRadiobutton.focus -children {
+		   ToolbarRadiobutton.label -sticky ns
+}    
+	}
+}			
+style element create ToolbarRadiobutton.indicator image [list $K(toolbutton_normal)\
+		{!active  !disabled !selected} $K(toolbutton_normal) \
+		{!active  !disabled selected} $K(toolbutton_selected) \
+		{active  !disabled selected} $K(toolbutton_selected) \
+		{active  !disabled !selected} $K(toolbutton_over) \
+		{disabled selected} $K(toolbutton_selected) \
+		{disabled !selected} $K(toolbutton_normal) ] \
+		-sticky nwes \
+		-border {3 3 3 3} \
+		-padding {0 0 0 0}
