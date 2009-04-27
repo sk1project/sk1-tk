9e677655ceda77a3aab90af8d5f4433bd19e9568
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2009-04-27 17:52:17 +0000

    package structure refactoring

--- src/app/UI/pluginpanels/__init__.py
+++ src/app/UI/pluginpanels/__init__.py
@@ -1,4 +1,1 @@
-import align_plugin, distribute_plugin
-import grid_plugin, guidelines_plugin
-import resize_plugin, move_plugin, rotate_plugin, skew_plugin, scale_plugin
-import find_replace_plugin, fill_plugin
+import effects, layout, transform, extentions, properties
