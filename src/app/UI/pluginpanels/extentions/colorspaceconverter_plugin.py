f3264145db9fa455d3d4645e0650220a1be595ef
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2009-05-02 19:21:21 +0000

    copyrights fix

--- src/app/UI/pluginpanels/extentions/colorspaceconverter_plugin.py
+++ src/app/UI/pluginpanels/extentions/colorspaceconverter_plugin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2008-2009 by Igor E. Novikov
+# Copyright (C) 2009 by Maxim S. Barabash
 #
 # This library is covered by GNU Library General Public License.
 # For more info see COPYRIGHTS file in sK1 root directory.
