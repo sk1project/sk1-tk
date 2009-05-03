5fac62f8b4214ab30dc1d1a74777abe23c1a0dc0
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2009-05-03 05:05:28 +0000

    Control Center initial implementation

---
+++ src/app/UI/cc/prefpanel.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2009 by Igor E. Novikov
+#
+# This library is covered by GNU Library General Public License.
+# For more info see COPYRIGHTS file in sK1 root directory.
+
+
+from Ttk import TFrame, TLabel, TButton
+from Tkinter import TOP,LEFT,RIGHT,BOTTOM,X,Y,BOTH,W,S,N,E,NORMAL,DISABLED,END
+from app import _, config
+import app
+
+class PrefPanel(TFrame):
+	
+	name='PreferencePanel'
+	title=_('Preference Panel')
+	icon='pref_plugin'
+	activated=0
+	contents=[]
+	
+	def init(self, master):
+		
+		TFrame.__init__(self, master)
+		
+		##### Title #########################
+		
+		self.title_label=TLabel(self, text=self.title, font=config.preferences.large_font, justify=LEFT)
+		self.title_label.pack(side=TOP, anchor=W)
+		
+		##### line #########################
+				
+		line = TLabel(self, style='HLine2')
+		line.pack(side = TOP, fill = X)				
+
+		##### here should be panel content #########################
+		
+		self.init_vars()		
+		self.build()
+		self.activated=1
+		
+	def init_vars(self):
+		#Copies preferences to object fields
+		pass
+		
+	def build(self):
+		#builds panel UI
+		pass
+	
+	def apply(self):
+		#Applies changes
+		pass
+	
