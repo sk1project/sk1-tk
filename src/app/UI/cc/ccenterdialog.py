e9b5b005957dd955b59ff12ed500a82c47bd1c0b
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2009-05-03 22:10:48 +0000

    test commit

--- src/app/UI/cc/ccenterdialog.py
+++ src/app/UI/cc/ccenterdialog.py
@@ -14,7 +14,7 @@ from app import dialogman
 from Ttk import TButton, TLabel, TFrame, TScrollbar
 from app.UI.ttk_ext import TSpinbox
 from Tkinter import Canvas
-from Tkinter import TOP,LEFT,RIGHT,BOTTOM,X,Y,BOTH,W,S,N,E,NORMAL,DISABLED,END, NONE
+from Tkinter import TOP, LEFT, RIGHT,BOTTOM,X,Y,BOTH,W,S,N,E,NORMAL,DISABLED,END, NONE
 
 from app.UI.widgets.scrolledcanvas import ScrolledCanvas
 from app.UI.widgets.treewidget import TreeItem, TreeNode
