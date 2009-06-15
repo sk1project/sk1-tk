6b4f03042c30ac76c24abfb7b33a0647f53cb13e
maxedrom <maxedrom@f60f84d3-f428-0410-86cb-e149298a0537>
2009-06-15 22:05:43 +0000

    added Index Color Palette and uses her in polyline

--- src/app/plugins/Filters/dxfloader.py
+++ src/app/plugins/Filters/dxfloader.py
@@ -41,6 +41,8 @@ from app.events.warn import INTERNAL, pdebug, warn_tb
 from app.io.load import GenericLoader, SketchLoadError
 import app
 
+from string import strip, atoi
+
 from math import sqrt, atan, atan2
 from math import pi, cos, sin
 
@@ -64,6 +66,287 @@ from app.conf.const import ArcArc, ArcChord, ArcPieSlice
 ##base_style.line_arrow2 = None
 ##base_style.font = None
 ##base_style.font_size = 12.0
+
+def csscolor(str):
+	str = str.strip()
+
+	if str[0] == '#':
+		if len(str) == 7:
+			r = atoi(str[1:3], 16) / 255.0
+			g = atoi(str[3:5], 16) / 255.0
+			b = atoi(str[5:7], 16) / 255.0
+		elif len(str) == 4:
+			# According to the CSS rules a single HEX digit is to be
+			# treated as a repetition of the digit, so that for a digit
+			# d the value is (16 * d + d) / 255.0 which is equal to d / 15.0
+			r = atoi(str[1], 16) / 15.0
+			g = atoi(str[2], 16) / 15.0
+			b = atoi(str[3], 16) / 15.0
+		color = CreateRGBColor(r, g, b)
+	elif namedcolors.has_key(str):
+		color = namedcolors[str]
+	else:
+		color = StandardColors.black
+	return color
+
+colors = {
+		0: csscolor('#000000'),
+		1: csscolor('#FF0000'),
+		2: csscolor('#FFFF00'),
+		3: csscolor('#00FF00'),
+		4: csscolor('#00FFFF'),
+		5: csscolor('#0000FF'),
+		6: csscolor('#FF00FF'),
+		7: csscolor('#FFFFFF'),
+		8: csscolor('#414141'),
+		9: csscolor('#808080'),
+		10: csscolor('#FF0000'),
+		11: csscolor('#FFAAAA'),
+		12: csscolor('#BD0000'),
+		13: csscolor('#BD7E7E'),
+		14: csscolor('#810000'),
+		15: csscolor('#815656'),
+		16: csscolor('#680000'),
+		17: csscolor('#684545'),
+		18: csscolor('#4F0000'),
+		19: csscolor('#4F3535'),
+		20: csscolor('#FF3F00'),
+		21: csscolor('#FFBFAA'),
+		22: csscolor('#BD2E00'),
+		23: csscolor('#BD8D7E'),
+		24: csscolor('#811F00'),
+		25: csscolor('#816056'),
+		26: csscolor('#681900'),
+		27: csscolor('#684E45'),
+		28: csscolor('#4F1300'),
+		29: csscolor('#4F3B35'),
+		30: csscolor('#FF7F00'),
+		31: csscolor('#FFD4AA'),
+		32: csscolor('#BD5E00'),
+		33: csscolor('#BD9D7E'),
+		34: csscolor('#814000'),
+		35: csscolor('#816B56'),
+		36: csscolor('#683400'),
+		37: csscolor('#685645'),
+		38: csscolor('#4F2700'),
+		39: csscolor('#4F4235'),
+		40: csscolor('#FFBF00'),
+		41: csscolor('#FFEAAA'),
+		42: csscolor('#BD8D00'),
+		43: csscolor('#BDAD7E'),
+		44: csscolor('#816000'),
+		45: csscolor('#817656'),
+		46: csscolor('#684E00'),
+		47: csscolor('#685F45'),
+		48: csscolor('#4F3B00'),
+		49: csscolor('#4F4935'),
+		50: csscolor('#FFFF00'),
+		51: csscolor('#FFFFAA'),
+		52: csscolor('#BDBD00'),
+		53: csscolor('#BDBD7E'),
+		54: csscolor('#818100'),
+		55: csscolor('#818156'),
+		56: csscolor('#686800'),
+		57: csscolor('#686845'),
+		58: csscolor('#4F4F00'),
+		59: csscolor('#4F4F35'),
+		60: csscolor('#BFFF00'),
+		61: csscolor('#EAFFAA'),
+		62: csscolor('#8DBD00'),
+		63: csscolor('#ADBD7E'),
+		64: csscolor('#608100'),
+		65: csscolor('#768156'),
+		66: csscolor('#4E6800'),
+		67: csscolor('#5F6845'),
+		68: csscolor('#3B4F00'),
+		69: csscolor('#494F35'),
+		70: csscolor('#7FFF00'),
+		71: csscolor('#D4FFAA'),
+		72: csscolor('#5EBD00'),
+		73: csscolor('#9DBD7E'),
+		74: csscolor('#408100'),
+		75: csscolor('#6B8156'),
+		76: csscolor('#346800'),
+		77: csscolor('#566845'),
+		78: csscolor('#274F00'),
+		79: csscolor('#424F35'),
+		80: csscolor('#3FFF00'),
+		81: csscolor('#BFFFAA'),
+		82: csscolor('#2EBD00'),
+		83: csscolor('#8DBD7E'),
+		84: csscolor('#1F8100'),
+		85: csscolor('#608156'),
+		86: csscolor('#196800'),
+		87: csscolor('#4E6845'),
+		88: csscolor('#134F00'),
+		89: csscolor('#3B4F35'),
+		90: csscolor('#00FF00'),
+		91: csscolor('#AAFFAA'),
+		92: csscolor('#00BD00'),
+		93: csscolor('#7EBD7E'),
+		94: csscolor('#008100'),
+		95: csscolor('#568156'),
+		96: csscolor('#006800'),
+		97: csscolor('#456845'),
+		98: csscolor('#004F00'),
+		99: csscolor('#354F35'),
+		100: csscolor('#00FF3F'),
+		101: csscolor('#AAFFBF'),
+		102: csscolor('#00BD2E'),
+		103: csscolor('#7EBD8D'),
+		104: csscolor('#00811F'),
+		105: csscolor('#568160'),
+		106: csscolor('#006819'),
+		107: csscolor('#45684E'),
+		108: csscolor('#004F13'),
+		109: csscolor('#354F3B'),
+		110: csscolor('#00FF7F'),
+		111: csscolor('#AAFFD4'),
+		112: csscolor('#00BD5E'),
+		113: csscolor('#7EBD9D'),
+		114: csscolor('#008140'),
+		115: csscolor('#56816B'),
+		116: csscolor('#006834'),
+		117: csscolor('#456856'),
+		118: csscolor('#004F27'),
+		119: csscolor('#354F42'),
+		120: csscolor('#00FFBF'),
+		121: csscolor('#AAFFEA'),
+		122: csscolor('#00BD8D'),
+		123: csscolor('#7EBDAD'),
+		124: csscolor('#008160'),
+		125: csscolor('#568176'),
+		126: csscolor('#00684E'),
+		127: csscolor('#45685F'),
+		128: csscolor('#004F3B'),
+		129: csscolor('#354F49'),
+		130: csscolor('#00FFFF'),
+		131: csscolor('#AAFFFF'),
+		132: csscolor('#00BDBD'),
+		133: csscolor('#7EBDBD'),
+		134: csscolor('#008181'),
+		135: csscolor('#568181'),
+		136: csscolor('#006868'),
+		137: csscolor('#456868'),
+		138: csscolor('#004F4F'),
+		139: csscolor('#354F4F'),
+		140: csscolor('#00BFFF'),
+		141: csscolor('#AAEAFF'),
+		142: csscolor('#008DBD'),
+		143: csscolor('#7EADBD'),
+		144: csscolor('#006081'),
+		145: csscolor('#567681'),
+		146: csscolor('#004E68'),
+		147: csscolor('#455F68'),
+		148: csscolor('#003B4F'),
+		149: csscolor('#35494F'),
+		150: csscolor('#007FFF'),
+		151: csscolor('#AAD4FF'),
+		152: csscolor('#005EBD'),
+		153: csscolor('#7E9DBD'),
+		154: csscolor('#004081'),
+		155: csscolor('#566B81'),
+		156: csscolor('#003468'),
+		157: csscolor('#455668'),
+		158: csscolor('#00274F'),
+		159: csscolor('#35424F'),
+		160: csscolor('#003FFF'),
+		161: csscolor('#AABFFF'),
+		162: csscolor('#002EBD'),
+		163: csscolor('#7E8DBD'),
+		164: csscolor('#001F81'),
+		165: csscolor('#566081'),
+		166: csscolor('#001968'),
+		167: csscolor('#454E68'),
+		168: csscolor('#00134F'),
+		169: csscolor('#353B4F'),
+		170: csscolor('#0000FF'),
+		171: csscolor('#AAAAFF'),
+		172: csscolor('#0000BD'),
+		173: csscolor('#7E7EBD'),
+		174: csscolor('#000081'),
+		175: csscolor('#565681'),
+		176: csscolor('#000068'),
+		177: csscolor('#454568'),
+		178: csscolor('#00004F'),
+		179: csscolor('#35354F'),
+		180: csscolor('#3F00FF'),
+		181: csscolor('#BFAAFF'),
+		182: csscolor('#2E00BD'),
+		183: csscolor('#8D7EBD'),
+		184: csscolor('#1F0081'),
+		185: csscolor('#605681'),
+		186: csscolor('#190068'),
+		187: csscolor('#4E4568'),
+		188: csscolor('#13004F'),
+		189: csscolor('#3B354F'),
+		190: csscolor('#7F00FF'),
+		191: csscolor('#D4AAFF'),
+		192: csscolor('#5E00BD'),
+		193: csscolor('#9D7EBD'),
+		194: csscolor('#400081'),
+		195: csscolor('#6B5681'),
+		196: csscolor('#340068'),
+		197: csscolor('#564568'),
+		198: csscolor('#27004F'),
+		199: csscolor('#42354F'),
+		200: csscolor('#BF00FF'),
+		201: csscolor('#EAAAFF'),
+		202: csscolor('#8D00BD'),
+		203: csscolor('#AD7EBD'),
+		204: csscolor('#600081'),
+		205: csscolor('#765681'),
+		206: csscolor('#4E0068'),
+		207: csscolor('#5F4568'),
+		208: csscolor('#3B004F'),
+		209: csscolor('#49354F'),
+		210: csscolor('#FF00FF'),
+		211: csscolor('#FFAAFF'),
+		212: csscolor('#BD00BD'),
+		213: csscolor('#BD7EBD'),
+		214: csscolor('#810081'),
+		215: csscolor('#815681'),
+		216: csscolor('#680068'),
+		217: csscolor('#684568'),
+		218: csscolor('#4F004F'),
+		219: csscolor('#4F354F'),
+		220: csscolor('#FF00BF'),
+		221: csscolor('#FFAAEA'),
+		222: csscolor('#BD008D'),
+		223: csscolor('#BD7EAD'),
+		224: csscolor('#810060'),
+		225: csscolor('#815676'),
+		226: csscolor('#68004E'),
+		227: csscolor('#68455F'),
+		228: csscolor('#4F003B'),
+		229: csscolor('#4F3549'),
+		230: csscolor('#FF007F'),
+		231: csscolor('#FFAAD4'),
+		232: csscolor('#BD005E'),
+		233: csscolor('#BD7E9D'),
+		234: csscolor('#810040'),
+		235: csscolor('#81566B'),
+		236: csscolor('#680034'),
+		237: csscolor('#684556'),
+		238: csscolor('#4F0027'),
+		239: csscolor('#4F3542'),
+		240: csscolor('#FF003F'),
+		241: csscolor('#FFAABF'),
+		242: csscolor('#BD002E'),
+		243: csscolor('#BD7E8D'),
+		244: csscolor('#81001F'),
+		245: csscolor('#815660'),
+		246: csscolor('#680019'),
+		247: csscolor('#68454E'),
+		248: csscolor('#4F0013'),
+		249: csscolor('#4F353B'),
+		250: csscolor('#333333'),
+		251: csscolor('#505050'),
+		252: csscolor('#696969'),
+		253: csscolor('#828282'),
+		254: csscolor('#BEBEBE'),
+		255: csscolor('#FFFFFF')
+		}
 
 def convert(code, value):
 	"""Convert a string to the correct Python type based on its dxf code.
@@ -117,6 +400,7 @@ class DXFLoader(GenericLoader):
 		self.update_trafo()
 		
 		self.curstyle = Style()
+
 
 	def update_trafo(self):
 		print self.INSUNITS, 'unit_to_pt', self.unit_to_pt
@@ -201,13 +485,16 @@ class DXFLoader(GenericLoader):
 
 	def polyline(self):
 		param={	'70': 0, # bit codes for Polyline entity
-				'40': 0.01
+				'40': 0.01, 
+				'62': None #color
 				}
 		param = self.read_param(param)
 		self.close_path = 0
 		self.path = CreatePath()
 		self.curstyle.line_width=param['40']*72
-		
+		if param['62'] is not None:
+			print 'colors'
+			self.curstyle.line_pattern = SolidPattern(colors[param['62']])
 		# if Group 70 Flag bit value set 1 This is a closed Polyline
 		self.close_path = param['70'] & 1 == 1
 
@@ -224,7 +511,7 @@ class DXFLoader(GenericLoader):
 			print '%%%%%%%%%%'
 			return
 		if param['42']==0:
-			print 'AppendLine',param
+			# print 'AppendLine',param
 			self.path.AppendLine(self.trafo(param['10'], param['20']))
 		else:
 			
