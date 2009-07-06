6f853c494e070a3b41bee2666241d10bb818dfd6
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2009-07-06 16:50:07 +0000

    template for new LCMS module interface

---
+++ src/app/Lib/cms.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2007 by Igor E. Novikov
+#
+# This library is covered by GNU Library General Public License.
+# For more info see COPYRIGHTS file in sK1 root directory.
+
+
+INTENT_PERCEPTUAL =             0
+INTENT_RELATIVE_COLORIMETRIC =  1
+INTENT_SATURATION =             2
+INTENT_ABSOLUTE_COLORIMETRIC =  3
+
+#cmsFLAGS_NOTPRECALC = ???
+#TYPE_RGB_8 = ???
+#TYPE_CMYK_8 = ???
+
+#def cmsOpenProfileFromFile():
+#def cmsCreateTransform():
+#def cmsCreateBitmapTransform():
+#def cmsDoTransform():
+#def cmsDoBitmapTransform():
+
+#def cmsDeleteTransform():
+#def cmsDeleteBitmapTransform():
+#def cmsCloseProfile():
