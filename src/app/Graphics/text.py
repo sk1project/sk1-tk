3d647ae9bb1284823393ec34666c73873f9d18f5
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2010-03-07 11:01:05 +0000

    import optimization

--- src/app/Graphics/text.py
+++ src/app/Graphics/text.py
@@ -96,8 +96,6 @@ from blend import Blend, MismatchError, BlendTrafo
 from properties import PropertyStack, FactoryTextStyle, DefaultTextProperties
 import color, pattern, app
 
-import graphics
-
 from app.Lib import encoding; iso_latin_1 = encoding.iso_latin_1
 
 
