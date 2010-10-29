7e3114727c62cccd5ab7238e2006599aa54cc436
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2010-10-29 18:01:18 +0000

    plugins browser initialization fix

--- src/app/UI/pluginpanels/pbrowser.py
+++ src/app/UI/pluginpanels/pbrowser.py
@@ -18,65 +18,72 @@ from ppanel import PluginPanel
 
 class PluginBrowser(PluginPanel):
 	
-	name='PluginBrowser'
+	name = 'PluginBrowser'
 	title = _("Plugin Browser")
-	icon='strip_pbrowser'
+	icon = 'strip_pbrowser'
+	
+	built = 0
 	
 	def init(self, master, pcontainer):
 		PluginPanel.init(self, master)
 		top = self.panel
-		self.pcontainer=pcontainer
-		ctheme=app.uimanager.currentColorTheme
+		self.pcontainer = pcontainer
+		ctheme = app.uimanager.currentColorTheme
 		
-		self.browserframe=TFrame(top, style='RoundedFrame', borderwidth=5)
+		self.browserframe = TFrame(top, style='RoundedFrame', borderwidth=5)
 		self.browserframe.pack(side=TOP, fill=BOTH, expand=1)
-		self.scanvas=ScrolledCanvas(self.browserframe, bg=ctheme.editfieldbackground, height=150, width=170)
+		self.scanvas = ScrolledCanvas(self.browserframe, bg=ctheme.editfieldbackground, height=150, width=170)
 		self.scanvas.frame.pack(side=TOP, fill=BOTH, expand=1)
 		self.closebut.forget()
-		self.build_plugins_tree()
-				
+		
+		self.after_idle(self.rebuild_pb)
+
+		
+	def rebuild_pb(self):
+		self.build_plugins_tree()				
+		ctheme = app.uimanager.currentColorTheme
 		item = PluginsTreeItem(self.ptree, self.pcontainer)		
 		node = TreeNode(self.scanvas.canvas, None, item, ctheme)
 		node.expand()
 		
 	def build_plugins_tree(self):
-		self.ptree=PluginCategory('ROOT', _("Plugins"))
-		objprop_group=PluginCategory('ObjProp', _("Object properties"))
-		layout_group=PluginCategory('Layout', _("Layout"))
-		transform_group=PluginCategory('Transform', _("Transformation"))
-		shaping_group=PluginCategory('Shaping', _("Shaping"))
-		effects_group=PluginCategory('Effects', _("Effects"))
-		extentions_group=PluginCategory('Extentions', _("Extensions"))
+		self.ptree = PluginCategory('ROOT', _("Plugins"))
+		objprop_group = PluginCategory('ObjProp', _("Object properties"))
+		layout_group = PluginCategory('Layout', _("Layout"))
+		transform_group = PluginCategory('Transform', _("Transformation"))
+		shaping_group = PluginCategory('Shaping', _("Shaping"))
+		effects_group = PluginCategory('Effects', _("Effects"))
+		extentions_group = PluginCategory('Extentions', _("Extensions"))
 
-		objprop_group.contents=app.objprop_plugins
-		layout_group.contents=app.layout_plugins
-		transform_group.contents=app.transform_plugins
-		shaping_group.contents=app.shaping_plugins
-		effects_group.contents=app.effects_plugins
-		extentions_group.contents=app.extentions_plugins
+		objprop_group.contents = app.objprop_plugins
+		layout_group.contents = app.layout_plugins
+		transform_group.contents = app.transform_plugins
+		shaping_group.contents = app.shaping_plugins
+		effects_group.contents = app.effects_plugins
+		extentions_group.contents = app.extentions_plugins
 		
-		self.ptree.contents+=[objprop_group,layout_group,transform_group,shaping_group,
-							effects_group,extentions_group]
+		self.ptree.contents += [objprop_group, layout_group, transform_group, shaping_group,
+							effects_group, extentions_group]
 			
 			
 
 	def collapse_panel(self, *arg):
 		PluginPanel.collapse_panel(self)
 		if self.collapsed:
-			self.pcontainer.spacer.canv_size=10
+			self.pcontainer.spacer.canv_size = 10
 #			self.pcontainer.spacer['width']=10
 			
 			
 class PluginCategory:
 	
-	name=''
-	title=''
-	icon='strip_category'
-	contents=[]
+	name = ''
+	title = ''
+	icon = 'strip_category'
+	contents = []
 	
-	def __init__(self,name,title):
-		self.name=name
-		self.title=title		
+	def __init__(self, name, title):
+		self.name = name
+		self.title = title		
 		
 		
 class PluginsTreeItem(TreeItem):
@@ -108,7 +115,7 @@ class PluginsTreeItem(TreeItem):
 	def GetSubList(self):
 		sublist = []
 		for name in self.objects.contents:
-			item = PluginsTreeItem(name,self.container)
+			item = PluginsTreeItem(name, self.container)
 			sublist.append(item)
 		return sublist
 	
