9aca6892eef56dc7466b0d05c59d43e6f7aa49f8
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2010-11-03 12:04:51 +0000

    code reformatted

--- src/app/UI/pluginpanels/properties/fill_plugin.py
+++ src/app/UI/pluginpanels/properties/fill_plugin.py
@@ -26,59 +26,59 @@ from math import floor, ceil
 from app.Graphics import color
 from app.Graphics.pattern import SolidPattern, EmptyPattern_
 
-BLACK_COLOR=color.CreateCMYKColor(0,0,0,1)
+BLACK_COLOR = color.CreateCMYKColor(0, 0, 0, 1)
 
 class FillPanel(PluginPanel):
-	name='SolidFill'
+	name = 'SolidFill'
 	title = _("Solid Fill")
-	initial_color=None
-	current_color=None
-	default_color=BLACK_COLOR
-	sign='tools_color_fill'
+	initial_color = None
+	current_color = None
+	default_color = BLACK_COLOR
+	sign = 'tools_color_fill'
 
 
 	def init(self, master):
 		PluginPanel.init(self, master)
 		
-		self.initial_color=self.default_color
+		self.initial_color = self.default_color
 		self.current_color = copy.copy(self.initial_color)
 
 		top = TFrame(self.panel, style='FlatFrame', borderwidth=5)
-		top.pack(side = TOP, fill=BOTH)
+		top.pack(side=TOP, fill=BOTH)
 
-		self.selector=ColorSpaceSelector(top, self.refresh_widgets, self.current_color, self.sign)
-		self.selector.pack(side=TOP, expand = 1, fill=X)
+		self.selector = ColorSpaceSelector(top, self.refresh_widgets, self.current_color, self.sign)
+		self.selector.pack(side=TOP, expand=1, fill=X)
 		
-		self.picker=ColorChooserWidget(top, self.refresh_widgets, self.current_color)
-		self.picker.pack(side=TOP, expand = 1, fill=X)	
+		self.picker = ColorChooserWidget(top, self.refresh_widgets, self.current_color)
+		self.picker.pack(side=TOP, expand=1, fill=X)	
 		
-		self.digitizer=ColorDigitizer(top, self.refresh_widgets, self.current_color)
-		self.digitizer.pack(side=TOP, expand = 1, fill=X)
+		self.digitizer = ColorDigitizer(top, self.refresh_widgets, self.current_color)
+		self.digitizer.pack(side=TOP, expand=1, fill=X)
 
 
-		button = UpdatedButton(top, text = _("Apply"),
-								command = self.apply_pattern,
-								sensitivecb = self.is_selection)
-		button.pack(side = BOTTOM, expand = 1, fill = X)
+		button = UpdatedButton(top, text=_("Apply"),
+								command=self.apply_pattern,
+								sensitivecb=self.is_selection)
+		button.pack(side=BOTTOM, expand=1, fill=X)
 		self.Subscribe(SELECTION, button.Update)
 		
 		button_frame = TFrame(top, style='FlatFrame', borderwidth=1)
-		button_frame.pack(side = BOTTOM, fill=X, pady=5)
+		button_frame.pack(side=BOTTOM, fill=X, pady=5)
 		
-		button = TButton(button_frame, style='TSmallbutton', text=' ', image='small_colorpicker', command = self.copy_from)
-		button.pack(side = LEFT)
+		button = TButton(button_frame, style='TSmallbutton', text=' ', image='small_colorpicker', command=self.copy_from)
+		button.pack(side=LEFT)
 		tooltips.AddDescription(button, _("Copy From..."))
 		
-		button = TButton(button_frame, style='TSmallbutton', text=' ', image='restore_color', command = self.restore_color)
-		button.pack(side = LEFT, padx=5)
+		button = TButton(button_frame, style='TSmallbutton', text=' ', image='restore_color', command=self.restore_color)
+		button.pack(side=LEFT, padx=5)
 		tooltips.AddDescription(button, _("Restore color"))
 		
 		self.var_autoupdate = IntVar(top)
 		self.var_autoupdate.set(1)
 		
-		self.autoupdate_check = TCheckbutton(button_frame, text = _("Auto Update"), variable = self.var_autoupdate, 
+		self.autoupdate_check = TCheckbutton(button_frame, text=_("Auto Update"), variable=self.var_autoupdate,
 											command=self.init_from_doc)
-		self.autoupdate_check.pack(side = RIGHT, anchor=W, padx=10)
+		self.autoupdate_check.pack(side=RIGHT, anchor=W, padx=10)
 
 		self.init_from_doc()
 		self.subscribe_receivers()
@@ -106,7 +106,7 @@ class FillPanel(PluginPanel):
 		self.refresh_widgets(self.current_color)
 	
 	def refresh_widgets(self, color):
-		self.current_color=color
+		self.current_color = color
 		self.selector.set_color(self.current_color)
 		self.picker.set_color(self.current_color)
 		self.digitizer.set_color(self.current_color)
@@ -132,7 +132,7 @@ class FillPanel(PluginPanel):
 	def get_object_color(self):
 		if self.document.HasSelection():
 			if len(self.document.selection) > 1:
-				info,obj=self.document.selection.GetInfo()[0]
+				info, obj = self.document.selection.GetInfo()[0]
 				properties = obj.Properties()
 			else:
 				properties = self.document.CurrentProperties()
@@ -146,5 +146,5 @@ class FillPanel(PluginPanel):
 		else:
 			return None
 
-instance=FillPanel()
+instance = FillPanel()
 app.objprop_plugins.append(instance)
