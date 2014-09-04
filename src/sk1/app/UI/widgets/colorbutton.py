695e8bd08558d4371702a43a886ae80be3fa62ce
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2014-09-04 18:54:55 +0000

    fix for ImageTk problem

--- src/sk1/app/UI/widgets/colorbutton.py
+++ src/sk1/app/UI/widgets/colorbutton.py
@@ -7,22 +7,33 @@
 #
 # The color sample size is 31x20 px
 
-from sk1sdk.libttk import TButton
+#from sk1sdk.libttk import TButton
+from sk1sdk.libtk.Tkinter import Button
 from PIL import Image, ImageTk
 
-class TColorButton(TButton):
-	
+def rgb_to_tkcolor(color):
+	"""
+	Converts list of RGB float values to hex color string.
+	For example: [1.0, 0.0, 1.0] => #ff00ff
+	"""
+	r, g, b = color
+	return '#%04x%04x%04x' % (int(255 * r), int(255 * g), int(255 * b))
+
+class TColorButton(Button):
+
 	def __init__(self, master=None, color=None, cnf={}, **kw):
-		TButton.__init__(self, master, kw)
-		self['style']='ColorButton'
+		Button.__init__(self, master, kw)
+		self['border'] = 1
+		self.bgc = self['bg']
 		self.set_color(color)
-		
+
 	def set_color(self, color):
 		if color is None:
-			self['image']='empty_pattern_colorbutton'
+			self['image'] = 'empty_pattern_colorbutton'
+			self['bg'] = self.bgc
+			self['activebackground'] = self.bgc
 		else:
-			#the color should be a rgb tuple like (10,20,30)
-			self.bitmap=Image.new("RGB",(31,20),color)
-			self.image=ImageTk.PhotoImage(self.bitmap)
-			self['image']=self.image
-			
+			self['image'] = 'pattern_colorbutton'
+			self['bg'] = rgb_to_tkcolor(color)
+			self['activebackground'] = rgb_to_tkcolor(color)
+
