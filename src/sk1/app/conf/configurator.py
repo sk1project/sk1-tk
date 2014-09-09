1313a1b4fbe327226e4dc73dc220918d9b52009a
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2014-09-09 11:35:39 +0000

    config refactored

--- src/sk1/app/conf/configurator.py
+++ src/sk1/app/conf/configurator.py
@@ -180,35 +180,70 @@ class Preferences(connector.Publisher):
 
 	#============== sK1 PREFERENCES ===================
 	sk1_version = ''
-	undo_limit = None#how many undo steps sketch remembers. None means unlimited.
-	system_encoding = 'utf-8'# default encoding for sK1 (GUI uses utf-8 only)
+	#How many undo steps sketch remembers. None means unlimited.
+	undo_limit = None
+	#Default encoding for sK1 (GUI uses utf-8 only)
+	system_encoding = 'utf-8'
 
-	#The initial grid geometry for a new document. It must be a tuple of the form (ORIG_X, ORIG_Y, WIDTH_X, WIDTH_Y). WIDTH_X and WIDTH_Y are
-	#the horizontal and the vertical distance between points of the grid, (ORIG_X, ORIG_X) is one point of the grid. These coordinates are given in Point
+	#============== NEW DOCUMENT PREFERENCES ===================
+	#The initial grid geometry for a new document. It must be a tuple of the
+	#form (ORIG_X, ORIG_Y, WIDTH_X, WIDTH_Y). WIDTH_X and WIDTH_Y are
+	#the horizontal and the vertical distance between points of the grid,
+	#(ORIG_X, ORIG_X) is one point of the grid.
+	#These coordinates are given in Point
 	grid_geometry = (0, 0, 2.83465, 2.83465)
 
-	#If the grid should be visible in a new document, set grid_visible to a true value
+	#If the grid should be visible in a new document,
+	#set grid_visible to a true value
 	grid_visible = 0
 
 	#Grid style: 0 - dotted; 1- lines
 	grid_style = 1
 
-	#The grid color of a new document as a tuple of RGB values in the range 0..1. E.g. (0, 0, 1) for blue
+	#The grid color of a new document as a tuple of RGB values in
+	#the range 0..1. E.g. (0, 0, 1) for blue
 	#grid_color = ('RGB', 0, 0, 1)
 	grid_color = ('RGB', 0.83, 0.87, 0.91)
 	grid_line_transparency = 0.5
 
-	#The outline color of a new GuideLayer as a tuple of RGB values in the range 0..1.
+	#The outline color of a new GuideLayer as a tuple of RGB values in
+	#the range 0..1. E.g. (0, 0, 1) for blue
 	guide_color = ('RGB', 0, 0.3, 1)
 	horizontal_guide_shape = [5, 7]
 	vertical_guide_shape = [5, 8]
 
 	layer_color = ('RGB', 0.196, 0.314, 0.635)
 
-	#When objects are duplicated, the new copies are translated by duplicate_offset, given in document coordiates
+	#Default paper format for new documents and documents read from a files
+	#that don't specify a paper format. This should be one of the formats
+	#defined in papersize.py.
+	default_paper_format = 'A4'
+
+	#Default page orientation. Portrait = 0, Landscape = 1.
+	#Other values are silently ignored.
+	default_page_orientation = 0
+
+	#----------Document font managment-------------
+	# The PS name of the font used for new text-objects
+	default_font = 'Arial'
+
+	system_font_dir = '/usr/share/fonts'
+
+	# should be expanded to absolute path
+	#If the font file for a font can't be found or if a requested font
+	#is not known at all, the fallback_font is used (PS name here):
+	user_font_dir = '.fonts'
+
+	fallback_font = 'BitstreamVeraSans-Roman'
+
+	#============== CANVAS PREFERENCES ===================
+
+	#When objects are duplicated, the new copies are translated by
+	#duplicate_offset, given in document coordinates
 	duplicate_offset = (10, 10)
 
-	#The default unit used in various places. Supported values: 'pt', 'in', 'cm', 'mm'
+	#The default unit used in various places.
+	#Supported values: 'pt', 'in', 'cm', 'mm'
 	default_unit = 'mm'
 	default_unit_jump = 0.1
 
@@ -224,110 +259,25 @@ class Preferences(connector.Publisher):
 	gradient_steps_editor = 100
 	gradient_steps_print = 50
 
-	#If the text on the screen becomes smaller than greek_threshold, don't render a font, but draw little lines instead. XXX see comments in graphics.py
+	#If the text on the screen becomes smaller than greek_threshold,
+	#don't render a font, but draw little lines instead.
+	#XXX see comments in graphics.py
 	greek_threshold = 5
 
-	#When snapping is active, coordinates specified with the mouse are snapped to the nearest `special' point (e.g. a grid point) if that is nearer than
-	#max_snap_distance pixels. (Thus, this length is given in window (pixel-) coordinates).
+	#When snapping is active, coordinates specified with the mouse are
+	#snapped to the nearest `special' point (e.g. a grid point)
+	#if that is nearer than max_snap_distance pixels.
+	#(Thus, this length is given in window (pixel-) coordinates).
 	max_snap_distance = 30
 
-	#If true and snapping is active, the current position displayed in the status bar is the position the mouse position would be snapped to.
+	#If true and snapping is active, the current position displayed in
+	#the status bar is the position the mouse position would be snapped to.
 	snap_current_pos = 1
 
 	#If true, change the cursor when above a selected object or a guide line
 	active_cursor = 1
 
-	#Icons
-	color_icons = 1
-
-	#List of most recently used files.
-	mru_files = ['', '', '', '', '']
-
 	viewport_ring_length = 10
-
-	#The standard palette. If this is a relative pathname it is
-	#interpreted relative to std_res_dir.
-	palette = 'standard.spl'
-	unipalette = 'CMYK_standart.skp'
-	arrows = 'standard.arrow'
-	dashes = 'standard.dashes'
-	pattern = 'pattern.ppm'
-
-	pattern_dir = ''
-	image_dir = ''
-
-	set_default_properties = 1# whether the apply button in the property dialogs sets the default properties for new objects. (   1 - do it, but ask; 0 - don't)
-	sample_text = 'Text'#	Font dialog sample text. Can be changed by simply editing it in the font dialog.
-	default_paper_format = 'A4'#Default paper format for new documents and documents read from a files that don't specify a paper format. This should be one of the formats defined in papersize.py.
-	default_page_orientation = 0#Default page orientation. Portrait = 0, Landscape = 1. Other values are silenty ignored.
-	draw_page_border = 1#TODO: Should be merged with show_page_outline!
-	page_border_size = 5
-
-	#Screen resolution in pixel per point. Used by the canvas to convert document coordinates to screen coordinates for a zoom factor of 100%
-	#None means to compute it from information obtained from the X-Server (ScreenWidth and ScreenMMWidth). 1.0 means 72 pixels per inch.
-	screen_resolution = 1.0
-
-	#If true, switch to selection mode after drawing an object. Stay in creation mode otherwise.
-	creation_is_temporary = 0
-
-	autoscroll_interval = 1# ms, 0 disables auto scrolling
-	autoscroll_amount = .5# no. of scroll units
-
-	#Ask user for confirmation if the memory size of an image is larger than huge_image_size (measured in bytes) (unused at the moment)
-	huge_image_size = 1 << 20
-
-	#Default resolution in pixels/inch for a new raster image that doesn't specify it itself. (not implemented yet)
-	default_image_resolution = 72
-
-	#The resoulution in pixel/inch of the preview image sK1 renders for preview. (using gs). Leave this at 72 for now.
-	eps_preview_resolution = 72
-
-	#Whether to print internal warning messages. Useful for debugging.
-	print_internal_warnings = 0
-
-	#print additional messages. these are usually only interesting for development purposes.
-	print_debug_messages = 0
-
-	#Howto report warnings to the user:   'dialog'	popup a dialog box;   'stderr'	write the message to stderr
-	warn_method = 'dialog'
-
-	#whether to show the special menu. The special menu contains some commands that provide access to sketch internals and new, experimental features.
-	show_special_menu = 0
-
-	show_advanced_snap_commands = 0#whether to show advanced snapping options.
-	activate_tooltips = 1#Use Tooltips.
-	tooltip_delay = 500#Delay for tooltips in milliseconds
-
-	window_title_template = '%(appname)s - [%(docname)s]'
-
-	panel_use_coordinates = 1#	If true, use the saved coordinates when opening a panel
-	panel_correct_wm = 1#	If true, try to compensate for the coordinate changes the window manager introduces by reparenting.
-	blend_panel_default_steps = 10
-	print_destination = 'printer'#	Default print destination. 'file' for file, 'printer' for printer
-
-	menu_tearoff_fix = 1#	Menus
-	drawing_precision = 3
-
-	#---------UI managment---------
-	style = 'Plastik'
-#	style = 'Clearlooks'
-#	style = 'eXPect'
-
-	color_theme = 'built-in'
-#	color_theme = 'UbuntuLooks'
-#	color_theme = 'eXPect'
-#	color_theme = 'ClassicPlastik'
-
-	icons = 'CrystalSVG'
-#	icons='eXPect'
-#	icons='Tango'
-#	icons='Human'
-
-	#---------UI fonts---------
-	small_font = 'Tahoma 8'
-	normal_font = 'Tahoma 9'
-	large_font = 'Tahoma 10 bold'
-	fixed_font = 'CourierNew 9'
 
 	#---------Color managment---------
 	user_rgb_profile = 0
@@ -344,13 +294,137 @@ class Preferences(connector.Publisher):
 	# 0 - use RGB, 1 - use CMYK
 	color_blending_rule = 1
 
-	#----------Document font managment-------------
-	default_font = 'BitstreamVeraSans-Roman'# The PS name of the font used for new text-objects
-	system_font_dir = '/usr/share/fonts'
-	user_font_dir = '.fonts'# should be expanded to absolute path
-	#If the font file for a font can't be found or if a requested font
-	#is not known at all, the fallback_font is used (PS name here):
-	fallback_font = 'BitstreamVeraSans-Roman'
+	#==========CAIRO DATA SECTION============
+	cairo_enabled = 1
+	alpha_channel_enabled = 1
+	bitmap_alpha_channel_enabled = 1
+	cairo_tolerance = .1
+	cairo_antialias = 0
+	cairo_bitmap_filter = 0
+
+	#==========PRINTING SECTION============
+	print_command = ''
+	pdf_level = '1.5'
+
+	#============== UI PREFERENCES ===================
+
+	window_title_template = '%(appname)s - [%(docname)s]'
+
+	#Icons
+	color_icons = 1
+
+	#Use Tooltips.
+	activate_tooltips = 1
+
+	#Delay for tooltips in milliseconds
+	tooltip_delay = 500
+
+	#---------UI managment---------
+	style = 'Plastik'
+	color_theme = 'built-in'
+	icons = 'CrystalSVG'
+
+	#---------UI fonts---------
+	small_font = 'Tahoma 8'
+	normal_font = 'Tahoma 9'
+	large_font = 'Tahoma 10 bold'
+	fixed_font = 'CourierNew 9'
+
+	#---------Ruler data---------
+	ruler_min_tick_step = 4
+	ruler_min_text_step = 33
+	ruler_max_text_step = 100
+
+	ruler_text_color = 'black'
+	ruler_tick_color = 'black'
+	ruler_color = '#F9F9FC'#Rulers background color
+
+	#============== APPLICATION PREFERENCES ===================
+
+	#List of most recently used files.
+	mru_files = ['', '', '', '', '']
+
+	#The standard palette. If this is a relative pathname it is
+	#interpreted relative to std_res_dir.
+	palette = 'standard.spl'
+	unipalette = 'CMYK_standart.skp'
+	arrows = 'standard.arrow'
+	dashes = 'standard.dashes'
+	pattern = 'pattern.ppm'
+
+	pattern_dir = ''
+	image_dir = ''
+
+	# whether the apply button in the property dialogs sets the default
+	#properties for new objects. (   1 - do it, but ask; 0 - don't)
+	set_default_properties = 1
+
+	#Font dialog sample text. Can be changed by simply editing it in the font dialog.
+	sample_text = 'Text'
+
+	#TODO: Should be merged with show_page_outline!
+	draw_page_border = 1
+	page_border_size = 5
+
+	#Screen resolution in pixel per point. Used by the canvas to convert
+	#document coordinates to screen coordinates for a zoom factor of 100%
+	#None means to compute it from information obtained from the X-Server
+	#(ScreenWidth and ScreenMMWidth). 1.0 means 72 pixels per inch.
+	screen_resolution = 1.0
+
+	#If true, switch to selection mode after drawing an object.
+	#Stay in creation mode otherwise.
+	creation_is_temporary = 0
+
+	# ms, 0 disables auto scrolling
+	autoscroll_interval = 1
+
+	# no. of scroll units
+	autoscroll_amount = .5
+
+	#Ask user for confirmation if the memory size of an image is larger than
+	#huge_image_size (measured in bytes) (unused at the moment)
+	huge_image_size = 1 << 20
+
+	#Default resolution in pixels/inch for a new raster image that doesn't
+	#specify it itself. (not implemented yet)
+	default_image_resolution = 72
+
+	#The resoulution in pixel/inch of the preview image sK1 renders for preview.
+	#(using gs). Leave this at 72 for now.
+	eps_preview_resolution = 72
+
+	#Whether to print internal warning messages. Useful for debugging.
+	print_internal_warnings = 0
+
+	#print additional messages. these are usually only interesting for
+	#development purposes.
+	print_debug_messages = 0
+
+	#Howto report warnings to the user:
+	#   'dialog'	popup a dialog box;
+	#   'stderr'	write the message to stderr
+	warn_method = 'dialog'
+
+	#whether to show the special menu. The special menu contains some commands
+	#that provide access to sketch internals and new, experimental features.
+	show_special_menu = 0
+
+	#whether to show advanced snapping options.
+	show_advanced_snap_commands = 0
+
+	#If true, use the saved coordinates when opening a panel
+	panel_use_coordinates = 1
+
+	#If true, try to compensate for the coordinate changes the window manager
+	#introduces by reparenting.
+	panel_correct_wm = 1
+
+	blend_panel_default_steps = 10
+	print_destination = 'printer'#	Default print destination. 'file' for file, 'printer' for printer
+
+	menu_tearoff_fix = 1#	Menus
+	drawing_precision = 3
 
 	#---------Open/save dialogs managment----
 	dir_for_open = '~'
@@ -363,15 +437,6 @@ class Preferences(connector.Publisher):
 	dialog_type = 2
 	#------------------------------------
 
-	#RULER data
-	ruler_min_tick_step = 4
-	ruler_min_text_step = 33
-	ruler_max_text_step = 100
-
-	ruler_text_color = 'black'
-	ruler_tick_color = 'black'
-	ruler_color = '#F9F9FC'#Rulers background color
-
 	color_cube = (6, 6, 6, 20)#	For PseudoColor displays.
 	reduce_color_flashing = 1
 
@@ -383,45 +448,42 @@ class Preferences(connector.Publisher):
 	#	Bezier Objects
 	#
 
-	#	Whether the first click-drag-release in the PolyLine creator
-	#	defines the start and end of the first line segment or just the
-	#	start point.
+	#Whether the first click-drag-release in the PolyLine creator
+	#defines the start and end of the first line segment or just the
+	#start point.
 	polyline_create_line_with_first_cklick = 1
 	topmost_is_mask = 1#	Mask Group
 
-	#How to insert from clipboard: 0 - on the same place; 1 - as a floating insertion
+	#How to insert from clipboard:
+	#	0 - on the same place;
+	#	1 - as a floating insertion
 	insertion_mode = 0
-	#How to insert imported graphics: 0 - on the same place; 1 - as a floating insertion
+
+	#How to insert imported graphics:
+	#	0 - on the same place;
+	#	1 - as a floating insertion
 	import_insertion_mode = 0
 
-	#	If true, try to unload some of the import filter modules after
-	#	use. Only filters marked as unloadable in their config file are
-	#	affected.
+	#If true, try to unload some of the import filter modules after
+	#use. Only filters marked as unloadable in their config file are
+	#affected.
 	unload_import_filters = 1
 
-	handle_jump = 1#Handle jump to manipulate objects by keyboard arrows (mm)
+	#Handle jump to manipulate objects by keyboard arrows (mm)
+	handle_jump = 1
 
-	editor_line_width = 1#   The line width for the outlines during a drag.
+	#The line width for the outlines during a drag.
+	editor_line_width = 1
 
-	#   Load these standard scripts at runtime in interactive mode. This
-	#   is really just a list of module names that are passed to
-	#   __import__, but don't count on it.
+	#Load these standard scripts at runtime in interactive mode. This
+	#is really just a list of module names that are passed to
+	#__import__, but don't count on it.
 	standard_scripts = ["app.scripts.export_raster", "app.scripts.simple_separation",
 						"app.scripts.spread", "app.scripts.reload_image",
 						"app.scripts.create_star", "app.scripts.create_star_outline",
 						"app.scripts.create_spiral", "app.scripts.read_gimp_path", ]
 
-	#==========CAIRO DATA SECTION============
-	cairo_enabled = 1
-	alpha_channel_enabled = 1
-	bitmap_alpha_channel_enabled = 1
-	cairo_tolerance = .1
-	cairo_antialias = 0
-	cairo_bitmap_filter = 0
 
-	#==========PRINTING SECTION============
-	print_command = ''
-	pdf_level = '1.5'
 
 class XMLPrefReader(handler.ContentHandler):
 	"""Handler for xml file reading"""
