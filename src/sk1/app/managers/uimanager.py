b6ffc688fb8411df8235049acb7845b6e243717d
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2014-09-10 09:32:30 +0000

    fix for font size

--- src/sk1/app/managers/uimanager.py
+++ src/sk1/app/managers/uimanager.py
@@ -26,7 +26,7 @@ class UIManager:
 
 		self.style = tkstyle.get_system_style(root)
 		self.currentColorTheme = self.style.colors
-		tkstyle.set_style(root, self.style, app.config.correct_font)
+		tkstyle.set_style(root, self.style, app.config.preferences.correct_font)
 		self.uploadExtentions()
 		self.defineCursors()
 
