cd45d32f2d336075a32c7cec8ba835c7afbfbfb9
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2014-10-08 16:40:15 +0000

    pluginpanel size fix

--- src/sk1/app/UI/pluginpanels/plugincontainer.py
+++ src/sk1/app/UI/pluginpanels/plugincontainer.py
@@ -6,7 +6,7 @@
 # For more info see COPYRIGHTS file in sK1 root directory.
 
 from sk1sdk.libttk import TFrame, TLabel
-from sk1sdk.libtk.Tkinter import TOP,LEFT,RIGHT,BOTTOM,X,Y,BOTH,W,S,N,E,NORMAL,DISABLED,END
+from sk1sdk.libtk.Tkinter import TOP, LEFT, RIGHT, BOTTOM, X, Y, BOTH, W, S, N, E, NORMAL, DISABLED, END
 from app import Publisher
 from app.conf.const import DOCUMENT, SELECTION, MODE
 from app.UI.widgets.resframe import ResizableTFrame
@@ -15,47 +15,47 @@ import app
 from pbrowser import PluginBrowser
 
 class PluginContainer(ResizableTFrame):
-	
-	visible=0
-	loaded=[]
-	activated=[]
-	
+
+	visible = 0
+	loaded = []
+	activated = []
+
 	def __init__(self, master, root, mw, cnf={}, **kw):
-		self.mw=mw
-		self.root=root
-		self.master=master
-		ResizableTFrame.__init__(self, master, root, size=200, orient=LEFT, min=200, max=300)
-		
+		self.mw = mw
+		self.root = root
+		self.master = master
+		ResizableTFrame.__init__(self, master, root, size=240, orient=LEFT, min=200, max=400)
+
 		b = TLabel(self.panel, style='HLine')
-		b.pack(side=BOTTOM, fill=X)	
-		
-		self.browserframe=ResizableTFrame(self.panel, root, size=10, orient=BOTTOM, min=10, max=500)
+		b.pack(side=BOTTOM, fill=X)
+
+		self.browserframe = ResizableTFrame(self.panel, root, size=10, orient=BOTTOM, min=10, max=500)
 		self.browserframe.pack(side=TOP, fill=X)
-		self.plugins=app.objprop_plugins+app.layout_plugins+app.transform_plugins
-		self.plugins+=app.effects_plugins+app.extentions_plugins+app.shaping_plugins
-		
-		self.pbrowser=PluginBrowser()	
-		
+		self.plugins = app.objprop_plugins + app.layout_plugins + app.transform_plugins
+		self.plugins += app.effects_plugins + app.extentions_plugins + app.shaping_plugins
+
+		self.pbrowser = PluginBrowser()
+
 	def showHide(self):
 		if not self.pbrowser.activated:
 			self.pbrowser.init(self.browserframe.panel, self)
 			self.pbrowser.forget()
 			self.pbrowser.pack(side=TOP, fill=BOTH, expand=1)
-			
+
 		if not self.visible:
-			self.visible=1
-			self.pack(side=RIGHT, fill=Y)						
+			self.visible = 1
+			self.pack(side=RIGHT, fill=Y)
 		else:
-			self.visible=0
+			self.visible = 0
 			self.forget()
-			self.master['width']=1
+			self.master['width'] = 1
 			self.mw.canvas.clear_buffer_bitmap()
 
-	def loadByName(self,name):
-		plugin=None
+	def loadByName(self, name):
+		plugin = None
 		for item in self.plugins:
-			if item.name==name:
-				plugin=item
+			if item.name == name:
+				plugin = item
 		if plugin is not None:
 			if plugin.activated:
 				if not plugin.packed:
@@ -64,4 +64,3 @@ class PluginContainer(ResizableTFrame):
 					plugin.collapse_panel()
 			else:
 				plugin.init(self.panel)
-		
