4e52ebe1aebcf5657167c12a2913dad587a5cdbe
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2014-10-27 16:34:06 +0000

    px unit added

--- src/app/Lib/units.py
+++ src/app/Lib/units.py
@@ -10,16 +10,23 @@
 # some factors to convert between different length units and the base
 # unit of sketch, PostScript points
 
+px_to_pt = 1.0
 in_to_pt = 72.0
 cm_to_pt = in_to_pt / 2.54
 mm_to_pt = cm_to_pt / 10
 m_to_pt	 = 100 * cm_to_pt
 
+pt_to_px = 1.0
 pt_to_in = 1.0 / 72.0
 pt_to_cm = 2.54 * pt_to_in
 pt_to_mm = pt_to_cm * 10
 pt_to_m	 = pt_to_cm / 100
 
 
-unit_dict = {'pt': 1.0, 'in': in_to_pt, 'cm': cm_to_pt, 'mm': mm_to_pt}
-unit_names = ['pt', 'in', 'cm', 'mm']
+unit_dict = {'px': px_to_pt,
+			'pt': 1.0,
+			'in': in_to_pt,
+			'cm': cm_to_pt,
+			'mm': mm_to_pt}
+
+unit_names = ['px', 'pt', 'in', 'cm', 'mm']
