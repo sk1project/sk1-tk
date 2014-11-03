81e38f85067a38f99d36c63d4037d525efd28465
igor_n <igor_n@f60f84d3-f428-0410-86cb-e149298a0537>
2014-11-03 12:33:30 +0000

    minor refactoring

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -71,14 +71,10 @@ IdentityMatrix = Identity.matrix()
 from _sketch import CreatePath, RectanglePath, RoundedRectanglePath, approx_arc, CreateFontMetric, SKCache, TransformRectangle
 from _sketch import ContAngle, ContSmooth, ContSymmetrical, SelNone, SelNodes, SelSegmentFirst, SelSegmentLast, Bezier, Line
 
-
-
 from events.skexceptions import *
 from events.undo import Undo, UndoList, CreateListUndo, CreateMultiUndo, UndoAfter, UndoRedo, NullUndo
 from events.connector import Connect, Disconnect, Issue, RemovePublisher, Subscribe, Publisher, QueueingPublisher
 
-
-#
 def updateInfo(inf1=None, inf2=None, inf3=None):
 	if not inf1 is None:
 		info1.set(inf1)
@@ -88,7 +84,6 @@ def updateInfo(inf1=None, inf2=None, inf3=None):
 		info3.set(inf3)
 	if not info_win is None:
 		info_win.update()
-
 
 command_classes = []
 
@@ -101,57 +96,39 @@ def RegisterCommands(aclass):
 # from Graphics.base import GraphicsObject, Primitive
 
 from Graphics.renderer import DocRenderer
-
 from Graphics.arrow import StandardArrows, Arrow
 from Graphics.properties import Style, FillStyle, EmptyFillStyle, LineStyle, EmptyLineStyle, PropertyStack, EmptyProperties
-
 from Graphics.blend import MismatchError, Blend, BlendTrafo
 from Graphics.blendgroup import BlendGroup, CreateBlendGroup, BlendInterpolation
-
 from Graphics.color import CreateRGBColor, XRGBColor, CreateCMYKColor, StandardColors, ParseSKColor
 from Graphics.compound import Compound, EditableCompound
 from Graphics.dashes import StandardDashes
-
 from Graphics.document import EditDocument, SelectionMode, EditMode
 Document = EditDocument
-
 from uc.libpango import get_fontface as GetFont
 from Graphics.gradient import MultiGradient, CreateSimpleGradient
 from Graphics.graphics import SimpleGC, GraphicsDevice, InvertingDevice, HitTestDevice
-
 from Graphics.group import Group
 from Graphics.guide import GuideLine
 from Graphics.image import Image, load_image, ImageData
 from Graphics.layer import Layer, GuideLayer, GridLayer
-
 from Graphics.maskgroup import MaskGroup
-
 from Graphics.pattern import EmptyPattern, SolidPattern, HatchingPattern, LinearGradient, RadialGradient, ConicalGradient, ImageTilePattern
-
 from Graphics.plugobj import PluginCompound, TrafoPlugin
-
 from Graphics.rectangle import Rectangle, RectangleCreator
 from Graphics.ellipse import Ellipse, EllipseCreator
 from Graphics.bezier import PolyBezier, PolyBezierCreator, PolyLineCreator, CombineBeziers, CreatePath, ContAngle, ContSmooth, ContSymmetrical
-
 from Graphics.psdevice import PostScriptDevice
-
-
 from Graphics.text import SimpleText, SimpleTextCreator, PathText
 
-
-def init_lib():
-	from uc import filters
-# 	config.load_user_preferences()
-	Issue(None, const.INITIALIZE)
+from uc import filters
 
 def init_ui():
 	# workaround for a threaded _tkinter in Python 1.5.2
 	if sys.version[:5] >= '1.5.2':
 		import paxtkinter
 		sys.modules['_tkinter'] = paxtkinter
-
-	init_lib()
+	Issue(None, const.INITIALIZE)
 
 def init_modules_from_widget(root):
 	import pax
